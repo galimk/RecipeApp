b6c0f0c328fc2b138c6667084ccc29c2561d48f1
galimk <galimk@gmail.com>
2022-07-19 07:53:29 -0600

    ability to add ingridients & instructions

--- ClientApp/src/components/RecipeCreateEdit.js
+++ ClientApp/src/components/RecipeCreateEdit.js
@@ -2,6 +2,57 @@ import {useHistory, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "axios";
 
+const RecipeRequiredObjectsList = ({list, propName, header, onListChange}) => {
+    const [newItem, setNewItem] = useState('');
+    return (
+        <>
+            <table className="table">
+                <tr>
+                    <th>{header}</th>
+                    <th></th>
+                </tr>
+                <tbody>
+                    {list.map((item, index) => (
+                        <tr key={index}>
+                            <td>
+                                {item[propName]}        
+                            </td>
+                            <td>
+                                <button type="button" className="btn btn-warning" onClick={() => {
+                                    if (window.confirm(`Delete ${header} ${item[propName]}?`)) {
+                                        list.splice(index, 1);
+                                        onListChange(list);
+                                    }
+                                }}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td>
+                            <div className="form-group">
+                                <input type="text" className="form-control" id="name" placeholder={`Add new ${header}`} onChange={(e ) => {
+                                    setNewItem(e.target.value)
+                                }} value={newItem}/>
+                            </div>
+                        </td>
+                        <td>
+                            <button className="btn btn-success" onClick={(e) => {
+                                e.preventDefault();
+                                onListChange([...list, {[propName]:newItem} ]);
+                                setNewItem('');
+                            }}>
+                                {`Add new ${header}`}
+                            </button>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </>
+    )
+};
+
 export const RecipeCreateEdit = () => {
     const {id} = useParams();
     const history = useHistory();
@@ -38,6 +89,14 @@ export const RecipeCreateEdit = () => {
                                    setRecipe({...recipe, description: e.target.value})
                                }}
                    value={recipe.description}/>
+                </div>
+                <div>
+                    <RecipeRequiredObjectsList list={recipe.instructions} propName={"text"} header={"Instructions"}
+                        onListChange={(newList) => setRecipe({...recipe, instructions: newList})}/>
+                </div>
+                <div>
+                    <RecipeRequiredObjectsList list={recipe.ingredients} propName={"name"} header={"Ingredients"}
+                       onListChange={(newList) => setRecipe({...recipe, ingredients: newList})}/>
                 </div>
                 <div style={{marginTop:"10px", float:"right"}}>
                     <button className="btn btn-success" onClick={(e) => {
